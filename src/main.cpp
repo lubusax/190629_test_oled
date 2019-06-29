b3f990982988c4290145479e22097ac828ef958f
lubusax <lu.bu.sax@gmail.com>
2019-06-29 19:16:34 +0200

    success "mensaje"

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,27 @@
 // The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8x8setupcpp
 // Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
 
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* clock=*/ 5, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE);
-
+//U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* clock=*/ 5, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE);
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
 
 void setup(void)
 {
-  
+  Serial.begin(115200);
   u8x8.begin();
-  u8x8.setPowerSave(0);
+  // u8x8.setPowerSave(0);
   
 }
 
 void loop(void)
 {
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0,0,"Hello World!");
-  u8x8.refreshDisplay();		// only required for SSD1606/7  
+  Serial.println("message one 111111111111111111");
+  u8x8.setFont(u8x8_font_inb21_2x4_f);
+  u8x8.setCursor(2, 0);
+  u8x8.print("mensaje");
+  
+  //u8x8.setFont(u8x8_font_chroma48medium8_r);
+  //u8x8.drawString(0,0,"Hello World!");
+  //u8x8.refreshDisplay();		// only required for SSD1606/7  
   delay(2000);
 }
