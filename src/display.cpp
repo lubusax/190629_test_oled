499d4d2255c4d4a4e46d29a914f00322d8039241
lubusax <lu.bu.sax@gmail.com>
2019-07-01 11:40:46 +0200

    cloudbuild only for esp8266

--- src/display.cpp
+++ src/display.cpp
@@ -9,7 +9,7 @@ U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE); // U8x8 Constru
 #define FONTMEDIUM_W   18 // width
 #define FONTMEDIUM_H   34 // height
 // #define FONTMEDIUM  u8x8_font_courB18_2x3_f
-#define FONTSMALL   u8x8_font_8x13B_1x2_f 
+#define FONTSMALL      u8x8_font_8x13B_1x2_f 
 
 void printBig (String msg)
 {
